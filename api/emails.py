3324530d8dd71fc80dd433a8df5b45103d41db6c
Tony <tonytong112358@gmail.com>
2024-10-12 15:25:32 -0700

    set up sqlite

--- api/emails.py
+++ api/emails.py
@@ -1,15 +1,22 @@
 from flask import Blueprint
 from flask.views import MethodView
+from flask import request,g
+from flask import jsonify
+import utils
+emails = Blueprint('emails', __name__)
 
 class emailsAPI(MethodView):
-    def get(self):
-        return "GET emails"
     def post(self):
-        return "POST emails"
-    def put(self):
-        return "PUT emails"
-    def delete(self):
-        return "DELETE emails"
-emails = Blueprint('emails', __name__)
+        data = request.get_json()
+        
+
+        # with g.db.cursor() as cur:
+        #     cur.execute("INSERT INTO emails (email) VALUES (%s)", (data['email'],))
+        print(data)
+        # return"hello"
+        return jsonify({"message": "Email added"}), 201
+
+emails.add_url_rule('/emails', view_func=emailsAPI.as_view('emailsAPI'),methods = ['POST'])
+
 
 
--- main.py
+++ main.py
@@ -1,7 +1,37 @@
 from flask import Flask 
 from flask import Blueprint
+from flask import g
+import sqlite3
+import os
+from api import emails
+
+con = sqlite3.connect('database.db')
 app = Flask(__name__)
 
+with open("schema.sql") as f:
+    con.executescript(f.read())
+
+
+@app.before_request
+def get_db():
+    if "db" not in g:
+        try:
+            con = sqlite3.connect('database.db')
+            g.db = con
+        except:
+            print("fail")
+    return con
+
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+app.register_blueprint(emails.emails,url_prefix='/api')
 if __name__ == '__main__':
     
-    app.run(debug=True)
+    # run the app the debug script in pipfile
+
+    app.run()
